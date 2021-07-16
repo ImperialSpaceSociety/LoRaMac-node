9637f5948a06fb302a7359287072032f69630f33
MedadRufus <medadrufus@gmail.com>
2021-07-16 01:11:16 +0100

    remove unused functions

--- src/peripherals/ublox/bsp.h
+++ src/peripherals/ublox/bsp.h
@@ -136,25 +136,6 @@ void BSP_sensor_Read( void );
 uint16_t BSP_GetSolarLevel16( void );
 
 
-/*!
- * Writes the given buffer to the EEPROM at the specified address.
- *
- * \param[IN] addr EEPROM address to write to
- * \param[IN] buffer Pointer to the buffer to be written.
- * \param[IN] size Size of the buffer to be written.
- * \retval status [SUCCESS, FAIL]
- */
-uint8_t EepromMcuWriteBuffer( uint16_t addr, uint8_t *buffer, uint16_t size );
-
-/*!
- * Reads the EEPROM at the specified address to the given buffer.
- *
- * \param[IN] addr EEPROM address to read from
- * \param[OUT] buffer Pointer to the buffer to be written with read data.
- * \param[IN] size Size of the buffer to be read.
- * \retval status [SUCCESS, FAIL]
- */
-uint8_t EepromMcuReadBuffer( uint16_t addr, uint8_t *buffer, uint16_t size );
 
 uint16_t get_time_pos_index_older_than(uint32_t minutes_from_epoch);
 void manage_incoming_instruction(uint8_t *instructions);
